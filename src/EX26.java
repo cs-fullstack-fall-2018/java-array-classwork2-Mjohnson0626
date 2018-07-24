78273110f5a5c4c30ca0b3aa926aa84edfcaf0cd
Mike <m.g.johnson0626@gmail.com>
2018-07-24 14:31:57 -0500

    check

---
+++ src/EX26.java
@@ -0,0 +1,12 @@
+//import java.util.ArrayList;
+//
+//public class EX26 {
+//    public static void main(String[] args){
+//        ArrayList<String>studentList = new ArrayList<String>();
+//        studentList.addAll(Arrays.asList("bob","john","kenn","kevin"));
+//    }
+//    public class StudentList(ArrayList<String> studentList){
+//
+//
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> squadList = new ArrayList<String>();
+        squadList.add("bob");
+        squadList.add("john");
+        squadList.add("kenn");
+        squadList.add("kevin");
+        squadList.remove("kenn");
+        System.out.println(squadList);
+        System.out.println(squadList.size());
+        System.out.println(squadList.get(2));
     }
 }
